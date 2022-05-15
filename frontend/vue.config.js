52293c4500f19eee9fb53ae197d93edc7580b98c
0gravity000 <0gravit000@gmail.com>
2022-05-15 23:45:04 +0900

    npm run build時のファイルの出力先を変更

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir:'../backend/dist', // npm run build 時のファイルの出力先ルート
 })
