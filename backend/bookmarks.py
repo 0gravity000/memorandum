9553b97d5a42c6b0863c53bfc85655b19fb1568e
0gravity000 <0gravit000@gmail.com>
2022-05-20 11:24:42 +0900

    bookmarksをBlueprintで分割

---
+++ backend/bookmarks.py
@@ -0,0 +1,158 @@
+from flask import Flask, jsonify, render_template, request
+import logging
+import json
+from google.cloud import datastore
+from datetime import date, datetime
+
+from flask import Blueprint
+
+# Instantiates a client
+client = datastore.Client()
+
+# Blueprintオブジェクトを生成
+bookmarks_bp = Blueprint('bookmarks', __name__, url_prefix='/api/bookmarks')
+
+@bookmarks_bp.route('/', methods=['GET', 'POST'])
+def bookmarks():
+    if request.method == 'POST':
+        logging.debug('now in post bookmarks')
+        json = request.get_json()
+        logging.debug(json)
+        # The kind for the new entity
+        kind = "Bookmark"
+        # The name/ID for the new entity
+        #name = "foo"
+        # 重複チェック
+        query = client.query(kind=kind)
+        result = list(query.add_filter('url', '=', json["url"]).fetch())
+        if result:
+            return "Already registered"
+
+        # The Cloud Datastore key for the new entity
+        bookmark_key = client.key(kind)
+        #bookmark_key = client.key(kind, name)
+        # Prepares the new entity
+        bookmark = datastore.Entity(key=bookmark_key)
+        bookmark["url"] = json["url"]
+        bookmark["title"] = json["title"]
+        bookmark["remarks"] = json["remarks"]
+        bookmark["updated_at"] = datetime.utcnow()
+        bookmark["created_at"] = datetime.utcnow()
+        logging.debug(bookmark)
+        # Saves the entity
+        client.put(bookmark)
+        logging.debug('now leave post bookmarks')
+        return bookmark
+    else:   #GET method
+        logging.debug('now in get bookmarks')
+        # The kind for the new entity
+        kind = "Bookmark"
+        query = client.query(kind=kind)
+        logging.debug(query)
+        result = list(query.fetch())
+        #logging.debug(result)
+        if not result:
+            logging.debug('now leave get bookmarks')
+            return ""
+
+        bookmarks = []
+        for item in result:
+            obj = dict(item)
+            #logging.debug(obj)
+            obj["id"] = item.key.id
+            #logging.debug(obj)
+            bookmarks.append(obj)
+
+        logging.debug('now leave get bookmarks')
+        return jsonify(bookmarks)
+        # logging.debug(jsonify(result))
+        # return jsonify(result)
+
+
+@bookmarks_bp.route('/show', methods=['GET'])
+def show_bookmark():
+    logging.debug('now in get bookmarks/show/<id>')
+    targetid = request.args.get('id')
+    logging.debug(targetid)
+    key = client.key("Bookmark", int(targetid))
+    result = client.get(key)
+    logging.debug(result)
+    if not result:
+        logging.debug('now leave get bookmarks/show/<id>')
+        return ""
+
+    obj = dict(result)
+    logging.debug(obj)
+    obj["id"] = result.key.id
+    logging.debug(obj)
+
+    logging.debug('now leave get bookmarks/show/<id>')
+    return jsonify(obj)
+
+
+@bookmarks_bp.route('/update/<targetid>', methods=['PUT'])
+def update_bookmark(targetid):
+    logging.debug('now in put bookmarks/update/<id>')
+    json = request.get_json()
+    logging.debug(json)
+    logging.debug(targetid)
+    key = client.key("Bookmark", int(targetid))
+    result = client.get(key)
+    result["url"] = json["url"]
+    result["title"] = json["title"]
+    result["remarks"] = json["remarks"]
+    result["updated_at"] = datetime.utcnow()
+    #result["created_at"] = datetime.utcnow()
+    logging.debug(result)
+    # Saves the entity
+    client.put(result)
+    logging.debug('now leave put bookmarks/update/<id>')
+
+    logging.debug(targetid)
+    key = client.key("Bookmark", int(targetid))
+    result = client.get(key)
+    logging.debug(result)
+    if not result:
+        logging.debug('now leave put bookmarks/update/<id>')
+        return ""
+
+    obj = dict(result)
+    logging.debug(obj)
+    obj["id"] = result.key.id
+    logging.debug(obj)
+
+    logging.debug('now leave put bookmarks/update/<id>')
+    return jsonify(obj)
+
+
+@bookmarks_bp.route('/delete/<targetid>', methods=['DELETE'])
+def delete_bookmark(targetid):
+    logging.debug('now in delete bookmarks/delete/<id>')
+    logging.debug(targetid)
+    key = client.key("Bookmark", int(targetid))
+    result = client.get(key)
+    logging.debug(result)
+    # delete the entity
+    client.delete(result)
+    logging.debug('now leave delete bookmarks')
+
+    # The kind for the new entity
+    kind = "Bookmark"
+    query = client.query(kind=kind)
+    logging.debug(query)
+    result = list(query.fetch())
+    logging.debug(result)
+    if not result:
+        logging.debug('now leave delete bookmarks')
+        return ""
+
+    bookmarks = []
+    for item in result:
+        obj = dict(item)
+        logging.debug(obj)
+        obj["id"] = item.key.id
+        logging.debug(obj)
+        bookmarks.append(obj)
+
+    logging.debug('now leave delete bookmarks')
+    return jsonify(bookmarks)
--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,13 @@ import json
 from google.cloud import datastore
 from datetime import date, datetime
 #from model import User, Bookmark, Tag, BookmarkUser, BookmarkTag
+from bookmarks import bookmarks_bp
 
 # instantiate the app
 app = Flask(__name__, static_folder='./dist/static', template_folder='./dist')
 #app = Flask(__name__)
 app.config.from_object(__name__)
+app.register_blueprint(bookmarks_bp)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
@@ -39,152 +41,6 @@ BOOKS = [
         'read': True
     }
 ]
-
-
-@app.route('/api/bookmarks', methods=['GET', 'POST'])
-def bookmarks():
-    if request.method == 'POST':
-        logging.debug('now in post bookmarks')
-        json = request.get_json()
-        logging.debug(json)
-        # The kind for the new entity
-        kind = "Bookmark"
-        # The name/ID for the new entity
-        #name = "foo"
-        # 重複チェック
-        query = client.query(kind=kind)
-        result = list(query.add_filter('url', '=', json["url"]).fetch())
-        if result:
-            return "Already registered"
-
-        # The Cloud Datastore key for the new entity
-        bookmark_key = client.key(kind)
-        #bookmark_key = client.key(kind, name)
-        # Prepares the new entity
-        bookmark = datastore.Entity(key=bookmark_key)
-        bookmark["url"] = json["url"]
-        bookmark["title"] = json["title"]
-        bookmark["remarks"] = json["remarks"]
-        bookmark["updated_at"] = datetime.utcnow()
-        bookmark["created_at"] = datetime.utcnow()
-        logging.debug(bookmark)
-        # Saves the entity
-        client.put(bookmark)
-        logging.debug('now leave post bookmarks')
-        return bookmark
-    else:   #GET method
-        logging.debug('now in get bookmarks')
-        # The kind for the new entity
-        kind = "Bookmark"
-        query = client.query(kind=kind)
-        logging.debug(query)
-        result = list(query.fetch())
-        #logging.debug(result)
-        if not result:
-            logging.debug('now leave get bookmarks')
-            return ""
-
-        bookmarks = []
-        for item in result:
-            obj = dict(item)
-            #logging.debug(obj)
-            obj["id"] = item.key.id
-            #logging.debug(obj)
-            bookmarks.append(obj)
-
-        logging.debug('now leave get bookmarks')
-        return jsonify(bookmarks)
-        # logging.debug(jsonify(result))
-        # return jsonify(result)
-
-
-@app.route('/api/bookmarks/show', methods=['GET'])
-def show_bookmark():
-    logging.debug('now in get bookmarks/show/<id>')
-    targetid = request.args.get('id')
-    logging.debug(targetid)
-    key = client.key("Bookmark", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    if not result:
-        logging.debug('now leave get bookmarks/show/<id>')
-        return ""
-
-    obj = dict(result)
-    logging.debug(obj)
-    obj["id"] = result.key.id
-    logging.debug(obj)
-
-    logging.debug('now leave get bookmarks/show/<id>')
-    return jsonify(obj)
-
-
-@app.route('/api/bookmarks/update/<targetid>', methods=['PUT'])
-def update_bookmark(targetid):
-    logging.debug('now in put bookmarks/update/<id>')
-    json = request.get_json()
-    logging.debug(json)
-    logging.debug(targetid)
-    key = client.key("Bookmark", int(targetid))
-    result = client.get(key)
-    result["url"] = json["url"]
-    result["title"] = json["title"]
-    result["remarks"] = json["remarks"]
-    result["updated_at"] = datetime.utcnow()
-    #result["created_at"] = datetime.utcnow()
-    logging.debug(result)
-    # Saves the entity
-    client.put(result)
-    logging.debug('now leave put bookmarks/update/<id>')
-
-    logging.debug(targetid)
-    key = client.key("Bookmark", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    if not result:
-        logging.debug('now leave put bookmarks/update/<id>')
-        return ""
-
-    obj = dict(result)
-    logging.debug(obj)
-    obj["id"] = result.key.id
-    logging.debug(obj)
-
-    logging.debug('now leave put bookmarks/update/<id>')
-    return jsonify(obj)
-
-
-@app.route('/api/bookmarks/delete/<targetid>', methods=['DELETE'])
-def delete_bookmark(targetid):
-    logging.debug('now in delete bookmarks/delete/<id>')
-    logging.debug(targetid)
-    key = client.key("Bookmark", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    # delete the entity
-    client.delete(result)
-    logging.debug('now leave delete bookmarks')
-
-    # The kind for the new entity
-    kind = "Bookmark"
-    query = client.query(kind=kind)
-    logging.debug(query)
-    result = list(query.fetch())
-    logging.debug(result)
-    if not result:
-        logging.debug('now leave delete bookmarks')
-        return ""
-
-    bookmarks = []
-    for item in result:
-        obj = dict(item)
-        logging.debug(obj)
-        obj["id"] = item.key.id
-        logging.debug(obj)
-        bookmarks.append(obj)
-
-    logging.debug('now leave delete bookmarks')
-    return jsonify(bookmarks)
 
 @app.route('/api/adjustBookmark', methods=['POST'])
 def adjust_bookmark():
