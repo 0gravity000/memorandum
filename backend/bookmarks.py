f066961c2f64e1fd5908b7a801bf485afb77dae5
0gravity000 <0gravit000@gmail.com>
2022-05-22 20:37:27 +0900

    Bookmark、Tagクラス化

--- backend/bookmarks.py
+++ backend/bookmarks.py
@@ -14,65 +14,32 @@ bookmarks_bp = Blueprint('bookmarks', __name__, url_prefix='/api/bookmarks')
 
 @bookmarks_bp.route('/', methods=['GET', 'POST'])
 def bookmarks():
+    bookmarks = Bookmark()
     if request.method == 'POST':    #POST Method
-        logging.debug('now in post bookmarks')
-        json = request.get_json()
-        logging.debug(json)
-        # The kind for the new entity
-        kind = "Bookmark"
-        # The name/ID for the new entity
-        #name = "foo"
-        # 重複チェック
-        query = client.query(kind=kind)
-        result = list(query.add_filter('url', '=', json["url"]).fetch())
-        if result:
-            return "Already registered"
-
-        # The Cloud Datastore key for the new entity
-        bookmark_key = client.key(kind)
-        #bookmark_key = client.key(kind, name)
-        # Prepares the new entity
-        bookmark = datastore.Entity(key=bookmark_key)
-        bookmark["url"] = json["url"]
-        bookmark["title"] = json["title"]
-        bookmark["remarks"] = json["remarks"]
-        bookmark["importance"] = int(json["importance"])
-        bookmark["updated_at"] = datetime.utcnow()
-        bookmark["created_at"] = datetime.utcnow()
-        logging.debug(bookmark)
-        # Saves the entity
-        logging.debug('★★★')
-        client.put(bookmark)
-        bookmarkid = bookmark.key.id
-        logging.debug(bookmarkid)
-
-        checkedTags = json["checkedTags"]
-        addlist = []
-        if checkedTags:
-            for checkedtag in checkedTags:
-                # 重複チェック
-                query = client.query(kind="BookmarkTags")
-                query.add_filter('bookmark_id', '=', int(bookmarkid))
-                query.add_filter('tag_id', '=', checkedtag)
-                result = list(query.fetch())
-                if result:
-                    continue
-                # The Cloud Datastore key for the new entity
-                bookmarktag_key = client.key("BookmarkTags")
-                # Prepares the new entity
-                bookmarktag = datastore.Entity(key=bookmarktag_key)
-                bookmarktag["bookmark_id"] = bookmarkid
-                bookmarktag["tag_id"] = checkedtag
-                bookmarktag["updated_at"] = datetime.utcnow()
-                bookmarktag["created_at"] = datetime.utcnow()
-                logging.debug(bookmarktag)
-                # Saves the entity
-                client.put(bookmarktag)
-
-        logging.debug('now leave post bookmarks')
-        return bookmark
-
+        return bookmarks.post_bookmark()
     else:   #GET method
+        return bookmarks.get_bookmarks()
+
+@bookmarks_bp.route('/show', methods=['GET'])
+def show_bookmark():
+    bookmarks = Bookmark()
+    return bookmarks.show_bookmark()
+
+@bookmarks_bp.route('/update/<targetid>', methods=['PUT'])
+def update_bookmark(targetid):
+    bookmarks = Bookmark()
+    return bookmarks.update_bookmark(targetid)
+
+@bookmarks_bp.route('/delete/<targetid>', methods=['DELETE'])
+def delete_bookmark(targetid):
+    bookmarks = Bookmark()
+    return bookmarks.delete_bookmark(targetid)
+
+class Bookmark():
+    def __init__(self):
+        pass
+
+    def get_bookmarks(self):
         logging.debug('now in get bookmarks')
         sortItem = request.args.get('sortItem')
         sortAsc = request.args.get('sortAsc')
@@ -135,116 +102,170 @@ def bookmarks():
         # logging.debug(jsonify(result))
         # return jsonify(result)
 
-@bookmarks_bp.route('/show', methods=['GET'])
-def show_bookmark():
-    logging.debug('now in get bookmarks/show/<id>')
-    targetid = request.args.get('id')
-    logging.debug(targetid)
-    key = client.key("Bookmark", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    if not result:
+    def post_bookmark(self):
+        logging.debug('now in post bookmarks')
+        json = request.get_json()
+        logging.debug(json)
+        # The kind for the new entity
+        kind = "Bookmark"
+        # The name/ID for the new entity
+        #name = "foo"
+        # 重複チェック
+        query = client.query(kind=kind)
+        result = list(query.add_filter('url', '=', json["url"]).fetch())
+        if result:
+            return "Already registered"
+
+        # The Cloud Datastore key for the new entity
+        bookmark_key = client.key(kind)
+        #bookmark_key = client.key(kind, name)
+        # Prepares the new entity
+        bookmark = datastore.Entity(key=bookmark_key)
+        bookmark["url"] = json["url"]
+        bookmark["title"] = json["title"]
+        bookmark["remarks"] = json["remarks"]
+        bookmark["importance"] = int(json["importance"])
+        bookmark["updated_at"] = datetime.utcnow()
+        bookmark["created_at"] = datetime.utcnow()
+        logging.debug(bookmark)
+        # Saves the entity
+        logging.debug('★★★')
+        client.put(bookmark)
+        bookmarkid = bookmark.key.id
+        logging.debug(bookmarkid)
+
+        checkedTags = json["checkedTags"]
+        addlist = []
+        if checkedTags:
+            for checkedtag in checkedTags:
+                # 重複チェック
+                query = client.query(kind="BookmarkTags")
+                query.add_filter('bookmark_id', '=', int(bookmarkid))
+                query.add_filter('tag_id', '=', checkedtag)
+                result = list(query.fetch())
+                if result:
+                    continue
+                # The Cloud Datastore key for the new entity
+                bookmarktag_key = client.key("BookmarkTags")
+                # Prepares the new entity
+                bookmarktag = datastore.Entity(key=bookmarktag_key)
+                bookmarktag["bookmark_id"] = bookmarkid
+                bookmarktag["tag_id"] = checkedtag
+                bookmarktag["updated_at"] = datetime.utcnow()
+                bookmarktag["created_at"] = datetime.utcnow()
+                logging.debug(bookmarktag)
+                # Saves the entity
+                client.put(bookmarktag)
+
+        logging.debug('now leave post bookmarks')
+        return bookmark
+
+    def show_bookmark(self):
+        logging.debug('now in get bookmarks/show/<id>')
+        targetid = request.args.get('id')
+        logging.debug(targetid)
+        key = client.key("Bookmark", int(targetid))
+        result = client.get(key)
+        logging.debug(result)
+        if not result:
+            logging.debug('now leave get bookmarks/show/<id>')
+            return ""
+
+        bookmarks = dict(result)
+        logging.debug(bookmarks)
+        bookmarks["id"] = result.key.id
+        logging.debug(bookmarks)
+        # タグを取得
+        query = client.query(kind='Tag')
+        query.order = ["name"]
+        result = list(query.fetch())
+        tags = add_keyid_queryresult(result)
+        logging.debug(tags)
+
+        bookmark_tags = show_bookmark_tags(targetid)
+        logging.debug(bookmark_tags)
+
         logging.debug('now leave get bookmarks/show/<id>')
-        return ""
+        return jsonify(bookmarks, tags, bookmark_tags)
 
-    bookmarks = dict(result)
-    logging.debug(bookmarks)
-    bookmarks["id"] = result.key.id
-    logging.debug(bookmarks)
-    # タグを取得
-    query = client.query(kind='Tag')
-    query.order = ["name"]
-    result = list(query.fetch())
-    tags = add_keyid_queryresult(result)
-    logging.debug(tags)
-
-    bookmark_tags = show_bookmark_tags(targetid)
-    logging.debug(bookmark_tags)
-
-    logging.debug('now leave get bookmarks/show/<id>')
-    return jsonify(bookmarks, tags, bookmark_tags)
-
-
-@bookmarks_bp.route('/update/<targetid>', methods=['PUT'])
-def update_bookmark(targetid):
-    logging.debug('now in put bookmarks/update/<id>')
-    json = request.get_json()
-    logging.debug(json)
-    logging.debug(targetid)
-    key = client.key("Bookmark", int(targetid))
-    result = client.get(key)
-    result["url"] = json["url"]
-    result["title"] = json["title"]
-    result["remarks"] = json["remarks"]
-    result["importance"] = int(json["importance"])
-    result["updated_at"] = datetime.utcnow()
-    #result["created_at"] = datetime.utcnow()
-    logging.debug(result)
-    # Saves the entity
-    client.put(result)
-    logging.debug('now leave put bookmarks/update/<id>')
-
-    #ブックマークを取得
-    logging.debug(targetid)
-    key = client.key("Bookmark", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    if not result:
+    def update_bookmark(self, targetid):
+        logging.debug('now in put bookmarks/update/<id>')
+        json = request.get_json()
+        logging.debug(json)
+        logging.debug(targetid)
+        key = client.key("Bookmark", int(targetid))
+        result = client.get(key)
+        result["url"] = json["url"]
+        result["title"] = json["title"]
+        result["remarks"] = json["remarks"]
+        result["importance"] = int(json["importance"])
+        result["updated_at"] = datetime.utcnow()
+        #result["created_at"] = datetime.utcnow()
+        logging.debug(result)
+        # Saves the entity
+        client.put(result)
         logging.debug('now leave put bookmarks/update/<id>')
-        return ""
 
-    bookmarks = dict(result)
-    logging.debug(bookmarks)
-    bookmarks["id"] = result.key.id
-    logging.debug(bookmarks)
+        #ブックマークを取得
+        logging.debug(targetid)
+        key = client.key("Bookmark", int(targetid))
+        result = client.get(key)
+        logging.debug(result)
+        if not result:
+            logging.debug('now leave put bookmarks/update/<id>')
+            return ""
 
-    #タグを取得
-    query = client.query(kind='Tag')
-    query.order = ["name"]
-    result = list(query.fetch())
-    #logging.debug(tags)
-    tags = add_keyid_queryresult(result)
-    #ブックマーク_タグを更新 
-    bookmark_tags = update_bookmark_tags(targetid, json["checkedTags"])
+        bookmarks = dict(result)
+        logging.debug(bookmarks)
+        bookmarks["id"] = result.key.id
+        logging.debug(bookmarks)
 
-    logging.debug('now leave put bookmarks/update/<id>')
-    return jsonify(bookmarks, tags, bookmark_tags)
+        #タグを取得
+        query = client.query(kind='Tag')
+        query.order = ["name"]
+        result = list(query.fetch())
+        #logging.debug(tags)
+        tags = add_keyid_queryresult(result)
+        #ブックマーク_タグを更新 
+        bookmark_tags = update_bookmark_tags(targetid, json["checkedTags"])
 
-@bookmarks_bp.route('/delete/<targetid>', methods=['DELETE'])
-def delete_bookmark(targetid):
-    logging.debug('now in delete bookmarks/delete/<id>')
-    logging.debug(targetid)
-    key = client.key("Bookmark", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    # delete the entity
-    client.delete(result)
-    logging.debug('now leave delete bookmarks/delete/<id>')
+        logging.debug('now leave put bookmarks/update/<id>')
+        return jsonify(bookmarks, tags, bookmark_tags)
 
-    #BookmarkTagsエンティティから削除
-    # The kind for the new entity
-    kind = "BookmarkTags"
-    query = client.query(kind=kind)
-    query.add_filter('bookmark_id', '=', int(targetid))
-    result = list(query.fetch())
-    logging.debug(result)
-    if result:
-        for item in result:
-            logging.debug(item)
-            client.delete(item)
-
-    #削除後、残りのentityを返す
-    # The kind for the new entity
-    kind = "Bookmark"
-    query = client.query(kind=kind)
-    logging.debug(query)
-    result = list(query.fetch())
-    logging.debug(result)
-    if not result:
+    def delete_bookmark(self, targetid):
+        logging.debug('now in delete bookmarks/delete/<id>')
+        logging.debug(targetid)
+        key = client.key("Bookmark", int(targetid))
+        result = client.get(key)
+        logging.debug(result)
+        # delete the entity
+        client.delete(result)
         logging.debug('now leave delete bookmarks/delete/<id>')
-        return ""
 
-    bookmarks = add_keyid_queryresult(result)
+        #BookmarkTagsエンティティから削除
+        # The kind for the new entity
+        kind = "BookmarkTags"
+        query = client.query(kind=kind)
+        query.add_filter('bookmark_id', '=', int(targetid))
+        result = list(query.fetch())
+        logging.debug(result)
+        if result:
+            for item in result:
+                logging.debug(item)
+                client.delete(item)
 
-    logging.debug('now leave delete bookmarks/delete/<id>')
-    return jsonify(bookmarks)
+        #削除後、残りのentityを返す
+        # The kind for the new entity
+        kind = "Bookmark"
+        query = client.query(kind=kind)
+        logging.debug(query)
+        result = list(query.fetch())
+        logging.debug(result)
+        if not result:
+            logging.debug('now leave delete bookmarks/delete/<id>')
+            return ""
+
+        bookmarks = add_keyid_queryresult(result)
+
+        logging.debug('now leave delete bookmarks/delete/<id>')
+        return jsonify(bookmarks)
--- backend/tags.py
+++ backend/tags.py
@@ -13,34 +13,31 @@ tags_bp = Blueprint('tags', __name__, url_prefix='/api/tags')
 
 @tags_bp.route('/', methods=['GET', 'POST'])
 def tags():
+    tags = Tag()
     if request.method == 'POST':    #POST Method
-        logging.debug('now in post tags')
-        json = request.get_json()
-        logging.debug(json)
-        # The kind for the new entity
-        kind = "Tag"
-        # The name/ID for the new entity
-        #name = "foo"
-        # 重複チェック
-        query = client.query(kind=kind)
-        result = list(query.add_filter('name', '=', json["name"]).fetch())
-        if result:
-            return "Already registered"
-
-        # The Cloud Datastore key for the new entity
-        tag_key = client.key(kind)
-        # Prepares the new entity
-        tag = datastore.Entity(key=tag_key)
-        tag["name"] = json["name"]
-        tag["remarks"] = json["remarks"]
-        tag["updated_at"] = datetime.utcnow()
-        tag["created_at"] = datetime.utcnow()
-        logging.debug(tag)
-        # Saves the entity
-        client.put(tag)
-        logging.debug('now leave post tags')
-        return tag
+        return tags.post_tag()
     else:   #GET method
+        return tags.get_tags()
+
+@tags_bp.route('/show', methods=['GET'])
+def show_tag():
+    tags = Tag()
+    return tags.show_tag()
+
+@tags_bp.route('/update/<targetid>', methods=['PUT'])
+def update_tag(targetid):
+    tags = Tag()
+    return tags.update_tag()
+
+@tags_bp.route('/delete/<targetid>', methods=['DELETE'])
+def delete_tag(targetid):
+    tags = Tag()
+    return tags.delete_tag()
+class Tag():
+    def __init__(self):
+        pass
+
+    def get_tags(self):
         logging.debug('now in get tags')
         # The kind for the new entity
         kind = "Tag"
@@ -68,107 +65,129 @@ def tags():
 
         logging.debug('now leave get tags')
         return jsonify(tags)
+    
+    def post_tag(self):
+        logging.debug('now in post tags')
+        json = request.get_json()
+        logging.debug(json)
+        # The kind for the new entity
+        kind = "Tag"
+        # The name/ID for the new entity
+        #name = "foo"
+        # 重複チェック
+        query = client.query(kind=kind)
+        result = list(query.add_filter('name', '=', json["name"]).fetch())
+        if result:
+            return "Already registered"
 
+        # The Cloud Datastore key for the new entity
+        tag_key = client.key(kind)
+        # Prepares the new entity
+        tag = datastore.Entity(key=tag_key)
+        tag["name"] = json["name"]
+        tag["remarks"] = json["remarks"]
+        tag["updated_at"] = datetime.utcnow()
+        tag["created_at"] = datetime.utcnow()
+        logging.debug(tag)
+        # Saves the entity
+        client.put(tag)
+        logging.debug('now leave post tags')
+        return tag
 
-@tags_bp.route('/show', methods=['GET'])
-def show_bookmark():
-    logging.debug('now in get tags/show/<id>')
-    targetid = request.args.get('id')
-    logging.debug(targetid)
-    key = client.key("Tag", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    if not result:
+    def show_tag():
+        logging.debug('now in get tags/show/<id>')
+        targetid = request.args.get('id')
+        logging.debug(targetid)
+        key = client.key("Tag", int(targetid))
+        result = client.get(key)
+        logging.debug(result)
+        if not result:
+            logging.debug('now leave get tags/show/<id>')
+            return ""
+
+        obj = dict(result)
+        logging.debug(obj)
+        obj["id"] = result.key.id
+        logging.debug(obj)
+
         logging.debug('now leave get tags/show/<id>')
-        return ""
+        return jsonify(obj)
 
-    obj = dict(result)
-    logging.debug(obj)
-    obj["id"] = result.key.id
-    logging.debug(obj)
-
-    logging.debug('now leave get tags/show/<id>')
-    return jsonify(obj)
-
-
-@tags_bp.route('/update/<targetid>', methods=['PUT'])
-def update_bookmark(targetid):
-    logging.debug('now in put tags/update/<id>')
-    json = request.get_json()
-    logging.debug(json)
-    logging.debug(targetid)
-    key = client.key("Tag", int(targetid))
-    result = client.get(key)
-    result["name"] = json["name"]
-    result["remarks"] = json["remarks"]
-    result["updated_at"] = datetime.utcnow()
-    #result["created_at"] = datetime.utcnow()
-    logging.debug(result)
-    # Saves the entity
-    client.put(result)
-    logging.debug('now leave put tags/update/<id>')
-
-    logging.debug(targetid)
-    key = client.key("Tag", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    if not result:
+    def update_tag(targetid):
+        logging.debug('now in put tags/update/<id>')
+        json = request.get_json()
+        logging.debug(json)
+        logging.debug(targetid)
+        key = client.key("Tag", int(targetid))
+        result = client.get(key)
+        result["name"] = json["name"]
+        result["remarks"] = json["remarks"]
+        result["updated_at"] = datetime.utcnow()
+        #result["created_at"] = datetime.utcnow()
+        logging.debug(result)
+        # Saves the entity
+        client.put(result)
         logging.debug('now leave put tags/update/<id>')
-        return ""
 
-    obj = dict(result)
-    logging.debug(obj)
-    obj["id"] = result.key.id
-    logging.debug(obj)
+        logging.debug(targetid)
+        key = client.key("Tag", int(targetid))
+        result = client.get(key)
+        logging.debug(result)
+        if not result:
+            logging.debug('now leave put tags/update/<id>')
+            return ""
 
-    logging.debug('now leave put tags/update/<id>')
-    return jsonify(obj)
+        obj = dict(result)
+        logging.debug(obj)
+        obj["id"] = result.key.id
+        logging.debug(obj)
 
+        logging.debug('now leave put tags/update/<id>')
+        return jsonify(obj)
 
-@tags_bp.route('/delete/<targetid>', methods=['DELETE'])
-def delete_bookmark(targetid):
-    logging.debug('now in delete tags/delete/<id>')
-    logging.debug(targetid)
-    key = client.key("Tag", int(targetid))
-    result = client.get(key)
-    logging.debug(result)
-    # delete the entity
-    client.delete(result)
-    logging.debug('now leave delete tags/delete/<id>')
-
-    #BookmarkTagsエンティティから削除
-    # The kind for the new entity
-    kind = "BookmarkTags"
-    query = client.query(kind=kind)
-    query.add_filter('tag_id', '=', int(targetid))
-    result = list(query.fetch())
-    logging.debug(result)
-    if result:
-        for item in result:
-            logging.debug(item)
-            client.delete(item)
-
-    #削除後、残りのentityを返す
-    # The kind for the new entity
-    kind = "Tag"
-    query = client.query(kind=kind)
-    logging.debug(query)
-    result = list(query.fetch())
-    logging.debug(result)
-    if not result:
+    def delete_tag(targetid):
+        logging.debug('now in delete tags/delete/<id>')
+        logging.debug(targetid)
+        key = client.key("Tag", int(targetid))
+        result = client.get(key)
+        logging.debug(result)
+        # delete the entity
+        client.delete(result)
         logging.debug('now leave delete tags/delete/<id>')
-        return ""
 
-    tags = add_keyid_queryresult(result)
-    '''
-    tags = []
-    for item in result:
-        obj = dict(item)
-        logging.debug(obj)
-        obj["id"] = item.key.id
-        logging.debug(obj)
-        tags.append(obj)
-    '''
+        #BookmarkTagsエンティティから削除
+        # The kind for the new entity
+        kind = "BookmarkTags"
+        query = client.query(kind=kind)
+        query.add_filter('tag_id', '=', int(targetid))
+        result = list(query.fetch())
+        logging.debug(result)
+        if result:
+            for item in result:
+                logging.debug(item)
+                client.delete(item)
 
-    logging.debug('now leave delete tags/delete/<id>')
-    return jsonify(tags)
+        #削除後、残りのentityを返す
+        # The kind for the new entity
+        kind = "Tag"
+        query = client.query(kind=kind)
+        logging.debug(query)
+        result = list(query.fetch())
+        logging.debug(result)
+        if not result:
+            logging.debug('now leave delete tags/delete/<id>')
+            return ""
+
+        tags = add_keyid_queryresult(result)
+        '''
+        tags = []
+        for item in result:
+            obj = dict(item)
+            logging.debug(obj)
+            obj["id"] = item.key.id
+            logging.debug(obj)
+            tags.append(obj)
+        '''
+
+        logging.debug('now leave delete tags/delete/<id>')
+        return jsonify(tags)
